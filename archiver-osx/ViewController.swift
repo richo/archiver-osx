5a8fb2e3f04d1ca23a9c213f983a6545de838d58
Richo Healey <richo@psych0tik.net>
2019-03-01 20:29:11 -0800

    Screw around more

--- archiver-osx/ViewController.swift
+++ archiver-osx/ViewController.swift
@@ -7,7 +7,7 @@
 
 import Cocoa
 
-var URL_BASE = "https://localhost:8000"
+var URL_BASE = "https://onatopp.psych0tik.net"
 
 class ViewController: NSViewController {
     @IBOutlet weak var username: NSTextField!
@@ -20,11 +20,15 @@ class ViewController: NSViewController {
         let errorMessage: String = String()
         NSLog("Trying to login with: %@:%@", username.stringValue, password.stringValue)
         
-        if var urlComponents = URLComponents(string: URL_BASE) {
-            urlComponents.path = "/json/login"
-            guard let url = urlComponents.url else {
-                return
-            }
+        var url = URLComponents(string: URL_BASE)!
+        url.path = "/json/login"
+        
+        var request = URLRequest(url: url.url!)
+        request.httpMethod = "POST"
+        request.setValue("application/x-www-form-urlencoded; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        request.HTTPBody = postData.dataUsingEncoding(NSUTF8StringEncoding)
+        var connection = NSURLConnection(request: request, delegate: nil, startImmediately: true)
+       
             dataTask = defaultSession.dataTask(with: url) { data, response, error in
                 defer { self.dataTask = nil }
                 
