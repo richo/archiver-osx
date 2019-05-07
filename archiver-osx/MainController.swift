943fd53b32ba4b01dd07cf206f100e8d34a11043
Richo Healey <richo@psych0tik.net>
2019-05-07 16:15:40 -0700

    Touched more things?

---
+++ archiver-osx/MainController.swift
@@ -0,0 +1,16 @@
+//
+//  MainController.swift
+//  archiver-osx
+//
+//  Created by richö butts on 5/6/19.
+//  Copyright © 2019 richö butts. All rights reserved.
+//
+
+import Cocoa
+import Foundation
+
+class MainController: NSViewController {
+    func showLoginModal(_ sender: Any) {
+    }
+    
+}
